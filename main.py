d4e189029e003c68f009a69af427a30e87446d2d
LinaMezghani <lina.mezghani@gmail.com>
2021-10-15 04:44:35 -0700

    add option for submitit

--- main.py
+++ main.py
@@ -1,157 +1,171 @@
 import argparse
 import datetime
 import gym
-import numpy as np
 import itertools
 import torch
-from sac import SAC
-from torch.utils.tensorboard import SummaryWriter
-from replay_memory import ReplayMemory
+import submitit
 
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+
+from sac import SAC
+from replay_memory import ReplayMemory
 from envs import make_sawyer_push_env
 
-parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
-parser.add_argument('--env-name', default="HalfCheetah-v2",
-                    help='Mujoco Gym environment (default: HalfCheetah-v2)')
-parser.add_argument('--policy', default="Gaussian",
-                    help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
-parser.add_argument('--eval', type=bool, default=True,
-                    help='Evaluates a policy a policy every 10 episode (default: True)')
-parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                    help='discount factor for reward (default: 0.99)')
-parser.add_argument('--tau', type=float, default=0.005, metavar='G',
-                    help='target smoothing coefficient(τ) (default: 0.005)')
-parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
-                    help='learning rate (default: 0.0003)')
-parser.add_argument('--alpha', type=float, default=0.2, metavar='G',
-                    help='Temperature parameter α determines the relative importance of the entropy\
-                            term against the reward (default: 0.2)')
-parser.add_argument('--automatic_entropy_tuning', type=bool, default=False, metavar='G',
-                    help='Automaically adjust α (default: False)')
-parser.add_argument('--seed', type=int, default=123456, metavar='N',
-                    help='random seed (default: 123456)')
-parser.add_argument('--batch_size', type=int, default=256, metavar='N',
-                    help='batch size (default: 256)')
-parser.add_argument('--num_steps', type=int, default=1000001, metavar='N',
-                    help='maximum number of steps (default: 1000000)')
-parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
-                    help='hidden size (default: 256)')
-parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
-                    help='model updates per simulator step (default: 1)')
-parser.add_argument('--start_steps', type=int, default=10000, metavar='N',
-                    help='Steps sampling random actions (default: 10000)')
-parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
-                    help='Value target update per no. of updates per step (default: 1)')
-parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
-                    help='size of replay buffer (default: 10000000)')
-parser.add_argument('--cuda', action="store_true",
-                    help='run on CUDA (default: False)')
-args = parser.parse_args()
+
 
 def process_obs(obs):
     return np.concatenate((obs['observation'], obs['desired_goal']))
 
-# Environment
-# env = NormalizedActions(gym.make(args.env_name))
+def main(args):
+    env = make_sawyer_push_env()
+    env.seed(args.seed)
+    env.action_space.seed(args.seed)
 
-#env = gym.make(args.env_name)
-env = make_sawyer_push_env()
-env.seed(args.seed)
-env.action_space.seed(args.seed)
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
 
+    # Agent
+    agent = SAC(env.observation_space['observation'].shape[0] * 2, env.action_space, args)
 
-torch.manual_seed(args.seed)
-np.random.seed(args.seed)
+    #Tesnorboard
+    writer = SummaryWriter('runs/{}_SAC_{}_{}_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args.env_name,
+                                                                 args.policy, "autotune" if args.automatic_entropy_tuning else ""))
 
-# Agent
-agent = SAC(env.observation_space['observation'].shape[0] * 2, env.action_space, args)
+    # Memory
+    memory = ReplayMemory(args.replay_size, args.seed)
 
-#Tesnorboard
-writer = SummaryWriter('runs/{}_SAC_{}_{}_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args.env_name,
-                                                             args.policy, "autotune" if args.automatic_entropy_tuning else ""))
+    # Training Loop
+    total_numsteps = 0
+    updates = 0
 
-# Memory
-memory = ReplayMemory(args.replay_size, args.seed)
+    for i_episode in itertools.count(1):
+        episode_reward = 0
+        episode_steps = 0
+        done = False
+        state = env.reset()
 
-# Training Loop
-total_numsteps = 0
-updates = 0
+        while not done:
+            if args.start_steps > total_numsteps:
+                action = env.action_space.sample()  # Sample random action
+            else:
+                action = agent.select_action(process_obs(state))  # Sample action from policy
 
-for i_episode in itertools.count(1):
-    episode_reward = 0
-    episode_steps = 0
-    done = False
-    state = env.reset()
+            if len(memory) > args.batch_size:
+                # Number of updates per step in environment
+                for i in range(args.updates_per_step):
+                    # Update parameters of all the networks
+                    critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory, args.batch_size, updates)
 
-    while not done:
-        if args.start_steps > total_numsteps:
-            action = env.action_space.sample()  # Sample random action
-        else:
-            action = agent.select_action(process_obs(state))  # Sample action from policy
+                    writer.add_scalar('loss/critic_1', critic_1_loss, updates)
+                    writer.add_scalar('loss/critic_2', critic_2_loss, updates)
+                    writer.add_scalar('loss/policy', policy_loss, updates)
+                    writer.add_scalar('loss/entropy_loss', ent_loss, updates)
+                    writer.add_scalar('entropy_temprature/alpha', alpha, updates)
+                    updates += 1
 
-        if len(memory) > args.batch_size:
-            # Number of updates per step in environment
-            for i in range(args.updates_per_step):
-                # Update parameters of all the networks
-                critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory, args.batch_size, updates)
+            next_state, reward, done, info = env.step(action) # Step
+            episode_steps += 1
+            total_numsteps += 1
+            episode_reward += reward
 
-                writer.add_scalar('loss/critic_1', critic_1_loss, updates)
-                writer.add_scalar('loss/critic_2', critic_2_loss, updates)
-                writer.add_scalar('loss/policy', policy_loss, updates)
-                writer.add_scalar('loss/entropy_loss', ent_loss, updates)
-                writer.add_scalar('entropy_temprature/alpha', alpha, updates)
-                updates += 1
+            # Ignore the "done" signal if it comes from hitting the time horizon.
+            # (https://github.com/openai/spinningup/blob/master/spinup/algos/sac/sac.py)
+            mask = 1 if episode_steps == env._max_episode_steps else float(not done)
 
-        next_state, reward, done, info = env.step(action) # Step
-        episode_steps += 1
-        total_numsteps += 1
-        episode_reward += reward
+            memory.push(process_obs(state), action, reward,
+                    process_obs(next_state), mask) # Append transition to memory
 
-        # Ignore the "done" signal if it comes from hitting the time horizon.
-        # (https://github.com/openai/spinningup/blob/master/spinup/algos/sac/sac.py)
-        mask = 1 if episode_steps == env._max_episode_steps else float(not done)
+            state = next_state
 
-        memory.push(process_obs(state), action, reward,
-                process_obs(next_state), mask) # Append transition to memory
+        if total_numsteps > args.num_steps:
+            break
 
-        state = next_state
+        writer.add_scalar('train/reward', episode_reward, i_episode)
+        writer.add_scalar('train/puck_dist', info['puck_distance'], i_episode)
+        print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}, puck_dist: {}".format(
+            i_episode, total_numsteps, episode_steps, round(episode_reward, 2),
+            round(info['puck_distance'], 2)
+        ))
 
-    if total_numsteps > args.num_steps:
-        break
+        if i_episode % 10 == 0 and args.eval is True:
+            avg_reward = 0.
+            avg_puck_dist = 0.
+            episodes = 10
+            for _  in range(episodes):
+                state = env.reset()
+                episode_reward = 0
+                done = False
+                while not done:
+                    action = agent.select_action(process_obs(state), evaluate=True)
 
-    writer.add_scalar('train/reward', episode_reward, i_episode)
-    writer.add_scalar('train/puck_dist', info['puck_distance'], i_episode)
-    print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}, puck_dist: {}".format(
-        i_episode, total_numsteps, episode_steps, round(episode_reward, 2),
-        round(info['puck_distance'], 2)
-    ))
+                    next_state, reward, done, info = env.step(action)
+                    episode_reward += reward
 
-    if i_episode % 10 == 0 and args.eval is True:
-        avg_reward = 0.
-        avg_puck_dist = 0.
-        episodes = 10
-        for _  in range(episodes):
-            state = env.reset()
-            episode_reward = 0
-            done = False
-            while not done:
-                action = agent.select_action(process_obs(state), evaluate=True)
+                    state = next_state
+                avg_reward += episode_reward
+                avg_puck_dist += info['puck_distance']
+            avg_reward /= episodes
+            avg_puck_dist /= episodes
 
-                next_state, reward, done, info = env.step(action)
-                episode_reward += reward
+            writer.add_scalar('test/avg_reward', avg_reward, i_episode)
+            writer.add_scalar('test/avg_puck-dist', avg_puck_dist, i_episode)
 
-                state = next_state
-            avg_reward += episode_reward
-            avg_puck_dist += info['puck_distance']
-        avg_reward /= episodes
-        avg_puck_dist /= episodes
+            print("----------------------------------------")
+            print("Test Episodes: {}, Avg. Reward: {}, Avg. Puck Dist: {}".format(
+                episodes, round(avg_reward, 2), round(avg_puck_dist, 2)))
+            print("----------------------------------------")
 
-        writer.add_scalar('test/avg_reward', avg_reward, i_episode)
-        writer.add_scalar('test/avg_puck-dist', avg_puck_dist, i_episode)
+    env.close()
 
-        print("----------------------------------------")
-        print("Test Episodes: {}, Avg. Reward: {}, Avg. Puck Dist: {}".format(
-            episodes, round(avg_reward, 2), round(avg_puck_dist, 2)))
-        print("----------------------------------------")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
+    parser.add_argument('--env-name', default="HalfCheetah-v2",
+                        help='Mujoco Gym environment (default: HalfCheetah-v2)')
+    parser.add_argument('--policy', default="Gaussian",
+                        help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
+    parser.add_argument('--eval', type=bool, default=True,
+                        help='Evaluates a policy a policy every 10 episode (default: True)')
+    parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                        help='discount factor for reward (default: 0.99)')
+    parser.add_argument('--tau', type=float, default=0.005, metavar='G',
+                        help='target smoothing coefficient(τ) (default: 0.005)')
+    parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
+                        help='learning rate (default: 0.0003)')
+    parser.add_argument('--alpha', type=float, default=0.2, metavar='G',
+                        help='Temperature parameter α determines the relative importance of the entropy\
+                                term against the reward (default: 0.2)')
+    parser.add_argument('--automatic_entropy_tuning', type=bool, default=False, metavar='G',
+                        help='Automaically adjust α (default: False)')
+    parser.add_argument('--seed', type=int, default=123456, metavar='N',
+                        help='random seed (default: 123456)')
+    parser.add_argument('--batch_size', type=int, default=256, metavar='N',
+                        help='batch size (default: 256)')
+    parser.add_argument('--num_steps', type=int, default=1000001, metavar='N',
+                        help='maximum number of steps (default: 1000000)')
+    parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
+                        help='hidden size (default: 256)')
+    parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
+                        help='model updates per simulator step (default: 1)')
+    parser.add_argument('--start_steps', type=int, default=10000, metavar='N',
+                        help='Steps sampling random actions (default: 10000)')
+    parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
+                        help='Value target update per no. of updates per step (default: 1)')
+    parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
+                        help='size of replay buffer (default: 10000000)')
+    parser.add_argument('--cuda', action="store_true",
+                        help='run on CUDA (default: False)')
+    parser.add_argument('--local', action="store_true",
+                        help='run locally (default: False)')
+    args = parser.parse_args()
 
-env.close()
+    if args.local:
+        main(args)
+
+    else:
+        submitit_log = 'submitit_logs/'
+        executor = submitit.AutoExecutor(folder=submitit_log)
+        executor.update_parameters(timeout_min=4320, partition="devlab",
+                gpus_per_node=1, name=exp_name)
+        job = executor.submit(learn_model, args)
+        print(job.job_id)
